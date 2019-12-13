5bc926962e02f73afade14c29eaf99e5c43c6935
Grzegorz Mazurek <grzegorz.mazurek86@gmail.com>
2019-12-13 12:42:07 +0100

    Deployment

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -149,18 +149,3 @@ main();
 //displayImages();
 //displayLastElement();
 
-// const authors = images.map(function (image) {
-//     return image.author;
-// });
-// console.log(authors);
-
-// const masterPrice = images.reduce(function (memory, image) {
-//     console.warn(memory);
-//     memory = memory + image.price;
-//     return memory;
-// }, 0);
-
-// const groupByPrice = images.reduce(function (memory, image) {
-// 	memory[image.price] = image;
-// 	return memory;
-// }, {})
